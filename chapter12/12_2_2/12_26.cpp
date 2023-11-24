bacc583aa177acdc6f94511f6c3a756af9bab6e6
Arrebol2020 <chzhou21@gmail.com>
2023-11-24 11:18:29 +0800

    add 12_2_2

---
+++ chapter12/12_2_2/12_26.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <memory>
+
+int main()
+{
+    std::allocator<int> alloc;
+    auto p = alloc.allocate(10);
+
+    auto q = p;
+
+    for (int i = 0; i < 10; i++)
+    {
+        alloc.construct(q++, i);
+    }
+
+    auto r = p;
+    while (q != r)
+    {
+        std::cout << *r << " ";
+        alloc.destroy(r++);
+    }
+
+    alloc.deallocate(p, 10);
+    std::cout << std::endl;
+    return 0;
+}
